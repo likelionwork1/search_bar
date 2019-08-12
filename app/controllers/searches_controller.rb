c56b00188bdc84db3215a239e34e56782158fc81
Ubuntu <ubuntu@ip-172-31-41-139.us-east-2.compute.internal>
2019-08-12 16:43:00 +0000

    done

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,5 @@
 class SearchesController < ApplicationController
   def result
-    @searches = eval(params[:bulletin]).where("title like ?", "%#{params[:search_text]}%")
-    # @searches = eval(params[:bulletin]).where("title = ?", params[:search_text])
+    @searches = eval(params[:model_name]).where("title like ?", "%#{params[:search_text]}%")
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create(title: "첫번째 게시글", content: "첫번째 내용입니다.")
+Post.create(title: "안녕", content: "두번째 내용입니다.")
+Post.create(title: "꽁꽁숨은 키워드", content: "찾을 수 있습니다.")
+Post.create(title: "안녕하세요", content: "단어가 포함만 되어도 같이 찾을수도 있습니다.")
+Post.create(title: "그럼 이만", content: "총총총...")
