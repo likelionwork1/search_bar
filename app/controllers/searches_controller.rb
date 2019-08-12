3c02e636569873b083c005228e57995a111dccc7
Ubuntu <ubuntu@ip-172-31-41-139.us-east-2.compute.internal>
2019-08-12 17:59:05 +0000

    done

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,10 @@
 class SearchesController < ApplicationController
   def result
+    @word = params[:search_text]
+
     @searches = eval(params[:model_name]).where("title like ?", "%#{params[:search_text]}%")
   end
 end
+
+
+
